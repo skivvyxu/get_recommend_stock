8c618f5624bee68e832c6a3d36d09684d3af20c4
Martin Xu <martin.xu.sz6@qq.com>
2017-01-01 17:08:04 +0800

    demo

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import chardet
 import re
 import json
 import sys
-from send_email import send_email
+from send_email import *
 import datetime
 
 # 设置默认编码格式为utf8
@@ -71,4 +71,76 @@ for i in xrange(length):
     email_text += "\n"
 
 # 发送邮件
-send_email(subject, email_text)
+#send_email(subject, email_text)
+#send_html_email(subject, email_text)
+
+#import tushare as ts
+#df = ts.shibor_data()
+
+import pygal
+from IPython.display import SVG, HTML
+
+html_pygal = """
+<!DOCTYPE html>
+<html>
+  <head>
+  <script type="text/javascript" src="http://kozea.github.com/pygal.js/javascripts/svg.jquery.js"></script>
+  <script type="text/javascript" src="http://kozea.github.com/pygal.js/javascripts/pygal-tooltips.js"></script>
+    <!-- ... -->
+
+  </head>
+  <body>
+    hello world
+  </body>
+</html>
+"""
+
+
+html_png = """
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <imag src="./test.png" >
+  </body>
+</html>
+"""
+
+
+line_chart = pygal.Line()
+line_chart.title = 'Browser usage evolution (in %)'
+line_chart.x_labels = map(str, range(2002, 2013))
+line_chart.add('Firefox', [None, None,    0, 16.6,   25,   31, 36.4, 45.5, 46.3, 42.8, 37.1])
+line_chart.add('Chrome',  [None, None, None, None, None, None,    0,  3.9, 10.8, 23.8, 35.3])
+line_chart.add('IE',      [85.8, 84.6, 84.7, 74.5,   66, 58.6, 54.7, 44.8, 36.2, 26.6, 20.1])
+line_chart.add('Others',  [14.2, 15.4, 15.3,  8.9,    9, 10.4,  8.9,  5.8,  6.7,  6.8,  7.5])
+
+longtext = html_pygal.format(pygal_render=line_chart.render())
+line_chart.render_to_png('./test.png')
+
+#send_html_email(subject, longtext)
+#send_html_email(subject, html_png)
+#send_html_email(subject, line_chart.render_response())
+#line_chart.render_to_file('test.svg')
+
+
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+
+with open('/home/pi/practise/python_learning/test1.ipynb') as f:
+    nb = nbformat.read(f, as_version=4)
+
+ep = ExecutePreprocessor(timeout=600)
+
+ep.preprocess(nb, {'metadata': {'path': '/home/pi/practise/python_learning'}})
+
+from traitlets.config import Config
+from nbconvert import HTMLExporter
+html_exporter = HTMLExporter()
+#html_exporter.template_file = 'basic'
+
+(body, resources) = html_exporter.from_notebook_node(nb)
+
+send_html_email(subject, body)
+
--- send_email.py
+++ send_email.py
@@ -3,6 +3,7 @@
 
 from email.header import Header
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 import smtplib
 
 # 导入配置文件，内部有邮箱配置信息
@@ -16,6 +17,22 @@ def send_email(subject, text, from_addr=From_addr, to_addr=To_addr, smtp_server=
     msg['To'] = to_addr
     msg['Subject'] = Header(subject, 'utf-8').encode()
     #server = smtplib.SMTP(smtp_server, 25)
+    server = smtplib.SMTP_SSL(smtp_server, 465)
+    server.login(from_addr, password)
+    server.sendmail(from_addr, [to_addr], msg.as_string())
+    server.quit()
+
+def send_html_email(subject, htmltext, from_addr=From_addr, to_addr=To_addr, smtp_server=Smtp_server, password=Password):
+    # the correct MIME type is multipart/alternative
+    msg = MIMEMultipart('alternative')
+    #msg = MIMEMultipart('multipart')
+    #msg = MIMEMultipart(htmltext, 'html', 'utf-8')
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    msg['Subject'] = Header(subject, 'utf-8').encode()
+    part1 = MIMEText(htmltext, 'html', 'utf-8')
+    #server = smtplib.SMTP(smtp_server, 25)
+    msg.attach(part1)
     server = smtplib.SMTP_SSL(smtp_server, 465)
     server.login(from_addr, password)
     server.sendmail(from_addr, [to_addr], msg.as_string())
